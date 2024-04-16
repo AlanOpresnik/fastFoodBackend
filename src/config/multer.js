9e3c75d3607f8ed5e9388e6b712b710c92de9dbb
Alan opresnik <alanopk328@gmail.com>
2024-04-16 15:10:35 -0300

    config multer destino

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const destinationPath = path.join(__dirname, "../uploadsProducts");
+    const destinationPath = path.join("src", "uploadsProducts");
     cb(null, destinationPath);
   },
   filename: function (req, file, cb) {
